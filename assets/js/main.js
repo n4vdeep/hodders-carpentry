df330210eb44941e74b98a44df48b3b2ae572b66
Nav Dhamu <n4vdeep@gmail.com>
2021-02-09 20:31:07 +0000

    added servicesd and updated client address

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Presento - v2.0.2
-* Template URL: https://bootstrapmade.com/presento-bootstrap-corporate-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
